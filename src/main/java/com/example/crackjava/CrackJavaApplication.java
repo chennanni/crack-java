66544526f5d4c89e98d69a25ad83b298ef81b520
Chennan <chennanni@users.noreply.github.com>
2023-11-17 20:01:24 +0800

    init commit

---
+++ src/main/java/com/example/crackjava/CrackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crackjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackJavaApplication.class, args);
+	}
+
+}
